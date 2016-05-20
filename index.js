ab5b4958ff483c1d50804d15b9c25cc0997ed06d
Dan Stineback <dstineback@gmail.com>
2016-05-20 09:27:31 -0700

    added image section

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var server = require('./server');
 var router = require('./router');
-requestHandlers = require('./requestHandlers');
+var requestHandlers = require('./requestHandlers');
 
 var handle = {};
 handle['/'] = requestHandlers.start;
 handle['/start'] = requestHandlers.start;
 handle['/upload'] = requestHandlers.upload;
+handle['/show'] = requestHandlers.show;
 
 server.start(router.route, handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,16 +1,18 @@
-var exec = require("child_process").exec;
+var querystring = require('querystring'),
+  fs = require('fs');
 
 function start(response, postData) {
   console.log("Request handler 'start' was called. ");
+
   var body = '<html>' +
   '<head>'+
   '<meta http-equiv="Content-Type" content="text/html;'+
   'charset=UTF-8" />'+
   '</head>'+
   '<body>'+
-  '<form action="/upload" method="post">'+
-  '<textarea name="text" rows="20" cols="60"></textarea>'+
-  '<input type="submit" value="Submit text" />'+
+  '<form action="/upload" enctype="multipart/form-data" + method="post">'+
+  '<input type="file" name="upload">'+
+  '<input type="submit" value="upload file" />'+
   '</form>'+
   '</body>'+
   '</html>';
@@ -23,9 +25,16 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called. ");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + postData);
+  response.write("You've sent the text: " + querystring.parse(postData).text);
   response.end();
+}
+
+function show(response) {
+  console.log('Request handler "show" was called.');
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
